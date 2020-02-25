b9cbd89ae7ec0be4503fd1a0746be2ad0dfb8208
jf-castanol <60269646+jf-castanol@users.noreply.github.com>
2020-02-24 22:17:45 -0500

    Fixing Model Class. Not Done

--- src/model/logic/Modelo.java
+++ src/model/logic/Modelo.java
@@ -1,13 +1,15 @@
 package model.logic;
 
-import java.io.FileReader;
+import java.io.*;
+
+import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 import model.data_structures.LinkedListImp;
 import model.data_structures.Multa;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import java.io.FileNotFoundException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,53 +22,78 @@ public class Modelo
 	LinkedListImp<Multa> lista = new LinkedListImp<>();
 	private Multa[] multasArr;
 
+	public LinkedListImp<Multa> ModeloJSON() throws FileNotFoundException {
+		Gson gson = new Gson();
+		BufferedReader br = null;
+		String path = "./data/comparendos_dei_2018_small.geojson";
+//		JsonReader reader;
 
+		try {
 
-	public LinkedListImp<Multa> ModeloJSON() throws FileNotFoundException
-	{
-		String path = "./data/comparendos_dei_2018.geojson";
-		JsonReader reader;
-
-		try{
-
-			reader = new JsonReader(new FileReader(path));
-			JsonElement elem = JsonParser.parseReader(reader);
-			JsonArray features = elem.getAsJsonObject().get("features").getAsJsonArray();
-			for(JsonElement e : features)
-			{
-				JsonElement properties = e.getAsJsonObject().get("properties");
-
-				String id = properties.getAsJsonObject().get("OBJECTID").getAsString();
-				String fechaHora = properties.getAsJsonObject().get("FECHA_HORA").getAsString();
-				String clase = properties.getAsJsonObject().get("CLASE_VEHI").getAsString();
-				String tipo = properties.getAsJsonObject().get("TIPO_SERVI").getAsString();
-				String infrac = properties.getAsJsonObject().get("INFRACCION").getAsString();
-				String descr = properties.getAsJsonObject().get("DES_INFRAC").getAsString();
-				String localidad = properties.getAsJsonObject().get("LOCALIDAD").getAsString();
-
-
-				List<Double> geo = new ArrayList<>();
-				if(e.getAsJsonObject().has("geometry") && !e.getAsJsonObject().get("geometry").isJsonNull())
-				{
-					for(JsonElement geoElem : e.getAsJsonObject().get("geometry").getAsJsonObject().get("coordinates").getAsJsonArray()){					
-						geo.add(geoElem.getAsDouble());
-					}
+			br = new BufferedReader(new FileReader(path));
+			FeatureCollection result = gson.fromJson(br, FeatureCollection.class);
+			CoordinateReferenceSystem coordRef = gson.fromJson(br, CoordinateReferenceSystem.class);
+			CRSProperty properties = gson.fromJson(br, CRSProperty.class);
+			Features feat = gson.fromJson(br, Features.class);
+			Properties propert = gson.fromJson(br, Properties.class);
+			GeometryData geo = gson.fromJson(br, GeometryData.class);
+			if (result != null) {
+				for (FeatureCollection features : result.features) {
+					Multa m = new Multa(propert.CLASE_VEHI, propert.TIPO_SERVI, propert.INFRACCION, propert.DES_INFRAC, propert.LOCALIDAD, propert.FECHA_HORA, geo.coordinates, propert.OBJECTID);
+					lista.insertarAlFinal(m);
 				}
-
-				Multa m = new Multa(clase, tipo, infrac, descr, localidad, fechaHora, geo, id);
-
-				lista.insertarAlFinal(m);
-
 			}
-		}catch (FileNotFoundException e){
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-
 		return lista;
-
 	}
 
 
+//			reader = new JsonReader(new FileReader(path));
+//			JsonElement elem = JsonParser.parseReader(reader);
+//			JsonArray features = elem.getAsJsonObject().get("features").getAsJsonArray();
+//			for(JsonElement e : features)
+//			{
+//				JsonElement properties = e.getAsJsonObject().get("properties");
+//
+//				String id = properties.getAsJsonObject().get("OBJECTID").getAsString();
+//				String fechaHora = properties.getAsJsonObject().get("FECHA_HORA").getAsString();
+//				String clase = properties.getAsJsonObject().get("CLASE_VEHI").getAsString();
+//				String tipo = properties.getAsJsonObject().get("TIPO_SERVI").getAsString();
+//				String infrac = properties.getAsJsonObject().get("INFRACCION").getAsString();
+//				String descr = properties.getAsJsonObject().get("DES_INFRAC").getAsString();
+//				String localidad = properties.getAsJsonObject().get("LOCALIDAD").getAsString();
+//
+//
+//				List<Double> geo = new ArrayList<>();
+//				if(e.getAsJsonObject().has("geometry") && !e.getAsJsonObject().get("geometry").isJsonNull())
+//				{
+//					for(JsonElement geoElem : e.getAsJsonObject().get("geometry").getAsJsonObject().get("coordinates").getAsJsonArray()){
+//						geo.add(geoElem.getAsDouble());
+//					}
+//				}
+//
+//				Multa m = new Multa(clase, tipo, infrac, descr, localidad, fechaHora, geo, id);
+//
+//				lista.insertarAlFinal(m);
+//
+//			}
+//		}catch (FileNotFoundException e){
+//			e.printStackTrace();
+//		}
+//
+//		return lista;
+//
+//	}
 
 
 	public Comparable<Multa>[] copiarComparendos(){
@@ -213,7 +240,38 @@ public class Modelo
 	private static void exch(Comparable<Multa>[] a, int i, int j)
 	{  Comparable<Multa> t = a[i]; a[i] = a[j]; a[j] = t;  }
 
+	//Inner Classes
 
+	static class FeatureCollection{
+		private String type;
+		private String name;
+		private CoordinateReferenceSystem crs ;
+		private FeatureCollection[] features;
+	}
+	static class CoordinateReferenceSystem{
+		private String type;
+		CRSProperty properties;
+	}
+	static class CRSProperty{
+		private String name;
+	}
+	static class Features{
+		private String type;
+	}
+	static class Properties{
+		private String OBJECTID;
+		private String FECHA_HORA;
+		private String MEDIO_DETE;
+		private String CLASE_VEHI;
+		private String TIPO_SERVI;
+		private String INFRACCION;
+		private String DES_INFRAC;
+		private String LOCALIDAD;
+	}
+	static class GeometryData{
+		private String type;
+		private ArrayList<Double> coordinates;
+	}
 }
 
 
